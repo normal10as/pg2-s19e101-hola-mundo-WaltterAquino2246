6b4ca5164e385a4b1ebe6690fe5c4bf448f4116c
WaltterAquino2246 <tewaaquino@gmail.com>
2019-04-29 20:20:27 -0300

    Add project files.

---
+++ Dias_de_la_semana/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim diasdelanio As UShort = 365
+        Dim diaslaborales As UShort
+        Dim diasemana As Byte = 7
+        Dim operacion As UShort
+        Dim semanaanio As UShort = diasdelanio / diasemana
+        Console.WriteLine("Dias del anio: " & " " & diasdelanio)
+        Console.WriteLine("semanas del anio: " & " " & semanaanio)
+
+        Console.Read()
+
+
+
+    End Sub
+
+End Module
---
+++ Dias_de_la_semana/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Dias_de_la_semana")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Dias_de_la_semana")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d32b7e06-9656-4e7f-b802-ea8feae9bea2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
